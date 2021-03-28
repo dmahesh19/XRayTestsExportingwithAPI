341591ddc5750ec288bb02a82b578cd3e69b5f13
dmahesh19 <mahesh.dm19@gmail.com>
2021-03-28 17:05:43 +0530

    Test Pro

---
+++ Utilities/testcasesimport.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from zipfile import ZipFile
+import shutil
+import os
+
+authheaderList = {"content-type":"application/json"}
+
+tokenUrl = "https://xray.cloud.xpand-it.com/api/v2/authenticate"
+
+featureUrl = "https://xray.cloud.xpand-it.com/api/v2/export/cucumber?keys=GF-11"
+
+file = open("..\creds.json",'r')
+json_input = file.read()
+#req_json = json.loads(json_input)
+
+#print(json_input)
+
+response = requests.post(tokenUrl,json_input,headers=authheaderList)
+
+#print(response.text)
+
+token = response.text[1:-1]
+
+#print(token)
+
+token = "Bearer "+token
+
+featureHeaderList={"Authorization":token}
+
+reponse1 = requests.get(featureUrl,headers=featureHeaderList)
+
+#print(reponse1.content)
+
+with open("..\downloads\d.zip","wb") as f:
+    f.write(reponse1.content)
+
+with ZipFile("..\downloads\d.zip",'r') as zip:
+    zip.extractall("..\\features")
+
+
+
--- features/1_GF-3.feature
+++
@@ -1,7 +0,0 @@
-Feature: Tes Case 1
-
-	@TEST_GF-3
-	Scenario: Tes Case 1
-		Given I am logged In
-		When Logged In
-		Then Success
