c41118ba2c17d118d6e598c93e1cc8810d6aadfc
dmahesh19 <mahesh.dm19@gmail.com>
2021-03-29 14:51:03 +0530

    Update testcasesimport.py

--- Utilities/testcasesimport.py
+++ Utilities/testcasesimport.py
@@ -10,19 +10,19 @@ tokenUrl = "https://xray.cloud.xpand-it.com/api/v2/authenticate"
 
 featureUrl = "https://xray.cloud.xpand-it.com/api/v2/export/cucumber?keys=GF-11"
 
-file = open("..\\creds.json",'r')
+file = open("..\creds.json",'r')
 json_input = file.read()
 #req_json = json.loads(json_input)
 
-#print(json_input)
+print(json_input)
 
 response = requests.post(tokenUrl,json_input,headers=authheaderList)
 
-#print(response.text)
+print(response.text)
 
 token = response.text[1:-1]
 
-#print(token)
+print(token)
 
 token = "Bearer "+token
 
@@ -30,13 +30,13 @@ featureHeaderList={"Authorization":token}
 
 reponse1 = requests.get(featureUrl,headers=featureHeaderList)
 
-#print(reponse1.content)
+print(reponse1.content)
 
-with open("..\\downloads\\d.zip","wb") as f:
+with open("..\downloads\d.zip","wb") as f:
     f.write(reponse1.content)
 
-with ZipFile("..\\downloads\\d.zip",'r') as zip:
-    zip.extractall("..\\features")
+with ZipFile("..\downloads\d.zip",'r') as zip:
+    zip.extractall("..\features")
 
 
 
